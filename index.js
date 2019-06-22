822888b1552966ef8623a12b6521b73d4eda51b1
Paula <paulaestayc@gmail.com>
2019-06-21 23:31:23 -0400

    read file

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
 module.exports = () => {
   // ...
 };
+
+
+
+const fs = require('fs');
+const marked = require('marked');
+const FileHound = require('filehound');
+ 
+
+fs.readFile('/etc/passwd', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+fs.readFile('./prueba.md','utf8', (err, data)=>{
+ if (err){
+    throw err;
+  }
+  console.log(data);
+});
+const files = (path) =>{
+  FileHound.create()
+  .paths(path)
+  .ext('md')
+  .find()
+ .then(files =>{
+files.forEach(file =>console.log('Found file', file));
+})
+};
+console.log(files('../SCL009-md-links'))
+
+const links = (path)=>{
+  fs.readdir(path,'utf8', (err, data)=>{
+if (err){
+  throw err;
+} 
+console.log(data)
+  })
+}
+console.log(links('../SCL009-md-links'))
+
+
+const links = (path)=>{
+  fs.readFile(path,'utf8', (err, data)=>{
+if (err){
+  throw err;
+} 
+let links = [];
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text){
+  links.push({
+    href:href,
+    text:text,
+    file:path
+  })
+}
+marked(data,{renderer:renderer});
+console.log(links)
+  })
+}
+console.log(links('./prueba.md'))
